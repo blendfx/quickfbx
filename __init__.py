e55039e291888350e2e51b9045fc7aed21a9b494
Simeon <simeon@noemis.de>
2020-10-20 19:45:08 +0200

    Update __init__.py
    improved the naming and export path

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ bl_info = {
     "name": "Quick Fbx",
     "description": "One button. One Fbx. Boom. It's that simple.",
     "author": "Blendfx, Jonas Dichelle, Simeon Conzendorf",
-    "version": (0, 0, 2),
+    "version": (0, 0, 3),
     "blender": (2, 80, 0),
     "location": "Properties > Scene",
     "category": "Export"
@@ -32,28 +32,43 @@ class FbxProps(PropertyGroup):
         maxlen=1024,
         subtype='DIR_PATH'
     )    
-    my_bool: bpy.props.BoolProperty(name="Option which does nothing yet")
+    b_forceKeys: bpy.props.BoolProperty(name="force start/end keyframes")
+    b_activeCol: bpy.props.BoolProperty(name="active colection",default=False)
+    b_leaveBones: bpy.props.BoolProperty(name="add leave bones",default=False)
+    b_filenameIsCollection: bpy.props.BoolProperty(name="name file like collection",default=false)
 
 def main(context):
     blendpath = Path(bpy.data.filepath)
-    if context.scene.fbx_props.fbx_path:
-        absolute = Path(bpy.path.abspath(context.scene.fbx_props.fbx_path))
-        fbxpath = Path(context.scene.fbx_props.fbx_path) / Path(blendpath).stem
+    print ("blendpath " + str(blendpath))    
+    filename = str (Path(blendpath).stem)
+    
+    if context.scene.fbx_props.fbx_path:        
+        fbxpath = str(Path(bpy.path.abspath(context.scene.fbx_props.fbx_path)) / Path(blendpath).stem)        
+        print ("abolute: "+fbxpath)
     else:
-        filename = str(Path(blendpath).parent / Path(blendpath).stem)
-        print("hello")
-        print(type(filename))
-        fbxpath = filename.replace(".","")
-        fbxpath = fbxpath+".fbx"
+        fbxpath = str(Path(blendpath).parent / Path(blendpath).stem)        
+        fbxpath = fbxpath.replace(".","")
+        print("relative "+fbxpath)
+    
+    if context.scene.fbx_props.b_activeCol:
+        collectionName = context.view_layer.active_layer_collection.name
+        
+        if context.scene.fbx_props.b_filenameIsCollection:
+            fbxpath = fbxpath.replace(filename,collectionName)
+        else:
+            fbxpath = fbxpath+"_"+collectionName
+
+    fbxpath = fbxpath+".fbx"
+    print(fbxpath +" "+filename)    
         
     bpy.ops.export_scene.fbx(filepath=str(fbxpath),
         check_existing=True,
         filter_glob="*.fbx",
         use_selection=False,
-        use_active_collection=False,
+        use_active_collection=context.scene.fbx_props.b_activeCol,
         global_scale=1.0,
         apply_unit_scale=True,
-        apply_scale_options='FBX_SCALE_NONE',
+        apply_scale_options='FBX_SCALE_ALL',
         bake_space_transform=False,
         object_types={'ARMATURE', 'EMPTY', 'MESH', 'OTHER'},
         use_mesh_modifiers=True,
@@ -63,7 +78,7 @@ def main(context):
         use_mesh_edges=False,
         use_tspace=False,
         use_custom_props=True,
-        add_leaf_bones=True,
+        add_leaf_bones=context.scene.fbx_props.b_leaveBones,
         primary_bone_axis='Y',
         secondary_bone_axis='X',
         use_armature_deform_only=False,
@@ -72,7 +87,7 @@ def main(context):
         bake_anim_use_all_bones=True,
         bake_anim_use_nla_strips=True,
         bake_anim_use_all_actions=True,
-        bake_anim_force_startend_keying=True,
+        bake_anim_force_startend_keying=context.scene.fbx_props.b_forceKeys,
         bake_anim_step=1.0,
         bake_anim_simplify_factor=1.0,
         path_mode='AUTO',
@@ -108,7 +123,11 @@ class Quick_fbx_panel(Panel):
         scene = context.scene
         layout.operator("wm.quick_fbx")
         layout.prop(props, "fbx_path", expand=True)
-        layout.prop(props, "my_bool", expand=True)
+        layout.prop(props, "b_forceKeys", expand=True)
+        layout.prop(props, "b_activeCol", expand=True)
+        layout.prop(props, "b_leaveBones", expand=True)
+        layout.prop(props, "b_filenameIsCollection", expand=True)       
+        
 
 def register():
     bpy.utils.register_class(QuickFBX)
